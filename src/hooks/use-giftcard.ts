68fdb4f595cdaf79704deb00800dc8c15aec45be
Seheon Yu <developer@seheon.kr>
2023-10-29 07:55:45 +0900

    Password input page and Giftcard page

---
+++ src/hooks/use-giftcard.ts
@@ -0,0 +1,34 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import GiftcardApi from '@/apis/giftcards';
+import Giftcard from '@/entities/giftcard';
+
+function useGiftcard(uuid: string, password: string) {
+  const [giftcard, setGiftcard] = useState<Giftcard | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function getGiftcard() {
+      try {
+        const giftcard = await GiftcardApi.getGiftcard(uuid, password);
+        setGiftcard(Giftcard.fromJson(giftcard));
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error);
+        } else {
+          setError(new Error('Unknown error'));
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    getGiftcard();
+  }, [uuid, password]);
+
+  return { giftcard, loading, error };
+}
+
+export default useGiftcard;
