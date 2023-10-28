d667e43ce7c31296ee002505cc689f398ccbdd4b
Seheon Yu <developer@seheon.kr>
2023-10-29 03:27:36 +0900

    Update the name of fetch instance to http

---
+++ src/apis/http.ts
@@ -0,0 +1,26 @@
+import returnFetch from 'return-fetch';
+
+const instance = returnFetch({
+  baseUrl: 'https://api.dev.gifthub.kr',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+const http = {
+  get: function (url: string, options?: RequestInit) {
+    return instance(url, {
+      method: 'GET',
+      ...options,
+    });
+  },
+  post: function (url: string, options?: RequestInit) {
+    return instance(url, {
+      method: 'POST',
+      ...options,
+    });
+  },
+};
+
+export { http };
