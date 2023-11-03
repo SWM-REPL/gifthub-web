e600689b8bfe24ce4bddbb4324301238af102666
Seheon Yu <developer@seheon.kr>
2023-11-03 13:16:49 +0900

    Update GiftHub landing page with under construction message and contact information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <main>
-      <p>GiftHub</p>
-    </main>
+    <div>
+      <h1 className='text-4xl'>GiftHub Landing Page</h1>
+      <p className='text-lg'>
+        Hi, this is the landing page for GiftHub. Our website is currently
+        under. construction. Please check back later!
+      </p>
+      <p>
+        Contact:{' '}
+        <a href='mailto:gifthub.swm@gmail.com'>gifthub.swm@gmail.com</a>
+      </p>
+    </div>
   );
 }
