5231e3c50e68943f1e7fb8c61652b7f38836b405
Seheon Yu <developer@seheon.kr>
2023-11-09 18:37:57 +0900

    Add new CDN hostname to next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.gifthub.kr',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dev.gifthub.kr',
+      },
     ],
   },
   async headers() {
