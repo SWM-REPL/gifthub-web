79343bb73b248adf0d26fb932620ab79775a0961
Seheon Yu <developer@seheon.kr>
2023-11-09 17:59:47 +0900

    Add headers for apple-app-site-association file.

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
