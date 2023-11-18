9f3b75a9dcd5aaaa95ce0daf9551abd7832c6bfa
Seheon Yu <developer@seheon.kr>
2023-11-19 02:44:13 +0900

    Update deeplink page styling

--- src/app/giftcards/[uuid]/deeplink/page.tsx
+++ src/app/giftcards/[uuid]/deeplink/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Page() {
   return (
-    <div className='w-screen text-center'>
+    <div className='flex flex-col items-center w-screen text-center'>
       <h3 className='text-lg mt-4 font-bold text-black'>
         기프트허브가 실행되지 않나요?
       </h3>
@@ -12,7 +12,7 @@ export default function Page() {
         최신버전으로 업데이트 해주세요.
       </p>
       <a href='https://play.google.com/store/apps/details?id=org.swmaestro.repl.GiftHub'>
-        <div className='mt-4 w-48 h-12 bg-red-500 rounded text-white'>
+        <div className='mt-4 bg-red-600 flex items-center justify-center rounded-md w-48 h-12 text-center text-white font-medium'>
           다운로드
         </div>
       </a>
